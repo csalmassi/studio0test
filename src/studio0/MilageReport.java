1b0fba63bcb36477200dcb94c37914aa600d2c6b
kensoe <kensoe@DESKTOP-C7S29ST.dhcp.wustl.edu>
2022-01-23 19:24:08 -0600

    Commit

--- src/studio0/MilageReport.java
+++ src/studio0/MilageReport.java
@@ -7,6 +7,7 @@ public class MilageReport {
 		int car2miles = 100;
 		int car3miles = 900;
 		
+		
 		int totalMiles = car1miles + car2miles + car3miles;
 		double milesPerGallon = 24.5;
 		double totalGallons = totalMiles / milesPerGallon;
